a49c68e70136d09cd0bfc463c617f4de83e4303c
Constantin Darea <dareaconstantin@gmail.com>
2019-02-02 16:50:22 +0200

    Homework for lesson v 0.2

--- lesson 5/CalcObelus.java
+++ lesson 5/CalcObelus.java
@@ -6,15 +6,15 @@ public class CalcObelus{
 	of two int variables, that user will insert. 
 	*/
 	public static void main(String[] args){
-		System.out.println("\nIn Order to calculete the division please insert a numbert");
-		Scanner scaner = new Scanner(System.in);
-		double firstNumber = scaner.nextDouble();
-		System.out.println("\nPlese insert the second number");
-		double secondNumber = scaner.nextDouble();
-		double divisionNumber = firstNumber / secondNumber;
-		System.out.println("\nThe answer is: " + divisionNumber);
 		System.out.println("\nDivision Calculator");
 		System.out.println("Author: Constantin Darea");
 		System.out.println("This application calculate the division of two integer number.");
+		System.out.println("\nIn Order to calculete the division please insert a numbert");
+		Scanner scaner = new Scanner(System.in);
+		int firstNumber = scaner.nextInt();
+		System.out.println("\nPlese insert the second number");
+		int secondNumber = scaner.nextInt();
+		double divisionNumber = (double) firstNumber / secondNumber;
+		System.out.println("\nThe answer is: " + divisionNumber);
 	}
 }
